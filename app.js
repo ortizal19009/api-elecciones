95db1297638b6cf9d038fa28df175e23b24e6cb7
Alexis Ortiz <83848928+ortizal19009@users.noreply.github.com>
2023-01-27 11:54:14 -0500

    install cors

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-/* const {conexion} = require('./db/conexion') */
+const cors = require("cors");
+const DIRECTORIO_PERMITIDO_CORS = "http://localhost:4200";
 app.use(express.json());
+app.use(cors({
+    origin: DIRECTORIO_PERMITIDO_CORS
+}));
 app.set('port',process.env.PORT || 3001)
 app.use("/api-elecciones", require("./routes"));
 app.listen(app.get('port'),()=>{
