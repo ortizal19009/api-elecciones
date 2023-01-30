a37c5765f76d3ae499241b65348095430c6002aa
Alexis Ortiz <83848928+ortizal19009@users.noreply.github.com>
2023-01-30 18:15:26 -0500

    up date changecs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const DIRECTORIO_PERMITIDO_CORS = "http://localhost:4200";
+const DIRECTORIO_PERMITIDO_CORS = "http://localhost:3000";
 app.use(express.json());
 app.use(cors({
     origin: DIRECTORIO_PERMITIDO_CORS
